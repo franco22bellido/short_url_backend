136b72d54df86feb85bce98d6653ec6c33bfab6b
franco22bellido <franco.bellido22@gmail.com>
2024-05-22 06:04:19 -0300

    cors

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -27,10 +27,9 @@ export const login = async (req, res) => {
     const payload = { userId: userFound._id, username: userFound.username }
 
     const token = jwt.sign(payload, keys.jwt_secret, { expiresIn: "24h" })
-
     res.cookie('token', token, {
         sameSite: "lax",
-        secure: false
+        secure: true
     })
 
     return res.status(200).json({
--- index.js
+++ index.js
@@ -2,23 +2,24 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import urlRoutes from './routes/url.route.js'
 import userRoutes from './routes/user.route.js'
-import {config} from 'dotenv'
+import { config } from 'dotenv'
 import keys from './common/entorn.variable.js';
-import {validateToken} from './middlewares/jwt.middleware.js';
+import { validateToken } from './middlewares/jwt.middleware.js';
 import cors from 'cors'
 import './db.js'
 config()
 
+
 const app = express();
-app.use(cors({credentials: true, origin: [keys.front_origin]}))
+app.use(cors({ credentials: true, origin: [keys.front_origin] }))
 app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 app.use('/url', validateToken, urlRoutes)
 app.use('/auth', userRoutes)
 
 
-app.listen(keys.port, ()=>{ 
+app.listen(keys.port, () => {
     console.log(`SERVER ON PORT ${keys.port}`)
 })
